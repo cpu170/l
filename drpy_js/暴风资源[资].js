dc543e8ad1318793b2b95da0b79f7008f8b250f3
cpu170 <125718856+cpu170@users.noreply.github.com>
2024-08-10 01:06:01 +0800

    Update 暴风资源[资].js

--- drpy_js/暴风资源[资].js
+++ drpy_js/暴风资源[资].js
@@ -3,7 +3,7 @@
 var rule = {
     模板: '采集1',
     title: '暴风资源[资]',
-    host: 'https://bfzyapi.com',
+    host: 'https://bfzy2.tv/',
     // homeTid: '13',
     homeTid: '',
     cate_exclude: '电影片|连续剧|综艺片|动漫片|电影解说|体育|演员|新闻资讯',
